3156da63d7404f27aaadb69a5f0bdb85ac0ec24e
Julian Pacheco <apacheco26@outlook.com>
2026-03-20 19:21:34 -0700

    re worked the order imported in main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # this makes it so only line of code that triggers run is main
+import owid
+import bea_pipeline
+import census_pipeline
+import nsrdb_pipeline
+import pvgis_pipeline
+import noaa_pipeline
+import eia_pipeline
+
 if __name__ == "__main__":
-    import eia_pipeline
-    import bea_pipeline
-    import census_pipeline
-    import nsrdb_pipeline
-    import pvgis_pipeline
-    import noaa_pipeline
+    pass
